b751bcac72b8432a58ebeb7ae82083462c302d92
Tanya <tanyarajhans7@gmail.com>
2022-01-21 01:13:44 +0530

    UI v1.0

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -25,9 +26,44 @@ class _HomeState extends State<Home> {
             ),
         ),
         Container(
+          padding: EdgeInsets.symmetric(vertical: 60.0, horizontal: 30.0),
         child: Column(
           children: <Widget>[
-
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: kIsWeb ? MainAxisAlignment.start : MainAxisAlignment.center,
+              children: [
+                Text('Cook', style: TextStyle(
+                  color: Colors.red[600],
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w500
+                )
+                ),
+                Text('Book', style: TextStyle(
+                  color: Colors.amber[900],
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w500
+                )
+                )
+              ],
+              
+            ),
+            SizedBox(
+                height: 40.0,
+            ),
+            Text('What will you cook today?', style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600
+                )),
+                SizedBox(
+                height: 8.0,
+            ),
+            Text('Enter the recipe you want to know about', style: TextStyle(
+                  color: Colors.blue[900],
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600
+                )),
         ],
        )
       ),
